eb4e5764b83bd71f86e5d3693db198d41ab0f4c3
umeboshi <umeboshi@80271e56-384c-46ef-8b4b-cf2c04a5ac6b>
2006-12-31 06:17:51 +0000

    add infodoc and widgets modules

---
+++ infodoc.py
@@ -0,0 +1,23 @@
+from forgetHTML import SimpleDocument
+from forgetHTML import Anchor, Table
+from forgetHTML import TableRow, TableCell
+from forgetHTML import TableHeader, Header
+
+# this is what separates parts of the url
+url_delimeter = '||'
+
+# handy function to split urls created in the text browser
+def split_url(url):
+    return str(url).split(url_delimeter)
+
+def make_url(*args):
+    return url_delimeter.join(args)
+
+class BaseDocument(SimpleDocument):
+    def __init__(self, title='BaseDocument', **args):
+        SimpleDocument.__init__(self, title=title)
+        self.maintable = Table(class_='BaseDocumentTable')
+        self.body.set(self.maintable)
+
+    def set_info(self, name):
+        pass
